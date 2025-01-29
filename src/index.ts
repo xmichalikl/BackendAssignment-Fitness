d321e435bcdcbccf128e929243e12f4171d31d3f
xmichalikl <xmichalikl@stuba.sk>
2025-01-29 17:39:43 +0100

    deprecated body-parser removed

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import http from 'http';
 import express from 'express';
-import * as bodyParser from 'body-parser';
 
-import { sequelize } from './config/sequelize';
-import ProgramRouter from './routes/programs';
-import ExerciseRouter from './routes/exercises';
+import { sequelize } from '@/config/sequelize';
 
+import ProgramRouter from '@/routes/programs';
+import ExerciseRouter from '@/routes/exercises';
+
+// Config
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Routes
 app.use('/programs', ProgramRouter());
 app.use('/exercises', ExerciseRouter());
 
+// HTTP server
 const httpServer = http.createServer(app);
 
+// Sync database
 sequelize.sync();
-
 console.log('Sync database', 'postgresql://localhost:5432/fitness_app');
 
+// Server start
 httpServer.listen(8000).on('listening', () => console.log(`Server started at port ${8000}`));
 
 export default httpServer;
